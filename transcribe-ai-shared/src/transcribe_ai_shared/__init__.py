62adf379830c5856ed84ba7472deacc8362b94dd
Neil <neil.rabia972972@gmail.com>
2026-07-21 17:24:13 +0200

    config(shared) : update import

--- transcribe-ai-shared/src/transcribe_ai_shared/__init__.py
+++ transcribe-ai-shared/src/transcribe_ai_shared/__init__.py
@@ -1,3 +1,4 @@
 """Shared components used by the Transcribe AI backend applications."""
-from transcribe_ai_shared.database import JobModel, JobRepositorie
+from transcribe_ai_shared.database import Job, JobRepository, JobStatus, JobType
 from transcribe_ai_shared.services import AudioManager, RedisQueueService
+from transcribe_ai_shared.BaseConfig import TranscribeAiBaseSettings
